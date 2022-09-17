733fa6461173d0a2b06f1064772af24358330629
florian <florian.carriere@gmail.com>
2022-09-17 17:06:23 +0200

    test another source

--- walker.js
+++ walker.js
@@ -1,32 +1,75 @@
 // Adapted from Dan Shiffman, natureofcode.com
 
-var Walker = function() {
-    this.x = width/2;
-    this.y = height/2;
-};
 
-Walker.prototype.display = function() {
-    stroke(0, 0, 0);
-    point(this.x, this.y);
-};
+let a = 80; // Create a global variable "a"
 
-// Randomly move up, down, left, right, or stay in one place
-Walker.prototype.walk = function() {
-    var choice = floor(random(4));
-    if (choice === 0) {
-        this.x++;
-    } else if (choice === 1) {
-        this.x--;
-    } else if (choice === 2) {
-        this.y++;
-    } else {
-        this.y--;
-    }
-};
+function setup() {
+  createCanvas(720, 400);
+  background(0);
+  stroke(255);
+  noLoop();
+}
 
-var w = new Walker();
+function draw() {
+  // Draw a line using the global variable "a"
+  line(a, 0, a, height);
 
-var draw = function() {
-    w.walk();
-    w.display();
-};
+  // Use a local variable a in for loop
+  for (let a = 120; a < 200; a += 3) {
+    line(a, 0, a, height);
+  }
+
+  // Make a call to the custom function drawAnotherLine()
+  drawAnotherLine();
+
+  // Make a call to the custom function drawYetAnotherLine()
+  drawYetAnotherLine();
+}
+
+function drawAnotherLine() {
+  // Create a new variable "a" local to this function
+  let a = 320;
+  // Draw a line using the local variable "a"
+  line(a, 0, a, height);
+}
+
+function drawYetAnotherLine() {
+  // Because no new local variable "a" is set,
+  // this line draws using the original global
+  // variable "a" which is set to the value 20.
+  line(a + 3, 0, a + 3, height);
+}
+
+
+//////////////
+
+// var Walker = function() {
+//     this.x = width/2;
+//     this.y = height/2;
+// };
+//
+// Walker.prototype.display = function() {
+//     stroke(0, 0, 0);
+//     point(this.x, this.y);
+// };
+//
+// // Randomly move up, down, left, right, or stay in one place
+// Walker.prototype.walk = function() {
+//     var choice = floor(random(4));
+//     if (choice === 0) {
+//         this.x++;
+//     } else if (choice === 1) {
+//         this.x--;
+//     } else if (choice === 2) {
+//         this.y++;
+//     } else {
+//         this.y--;
+//     }
+// };
+//
+// var w = new Walker();
+//
+// var draw = function() {
+//     w.walk();
+//     w.display();
+// };
