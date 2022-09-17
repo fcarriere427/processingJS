67fc41acda09b27ef959e78269706b31efafad15
florian <florian.carriere@gmail.com>
2022-09-17 17:11:18 +0200

    test walker

--- walker.js
+++ walker.js
@@ -1,75 +1,75 @@
-// Adapted from Dan Shiffman, natureofcode.com
-
-
-let a = 80; // Create a global variable "a"
+let width = 1500;
+let height = 750;
 
 function setup() {
-  createCanvas(1500, 750);
+  createCanvas(width, height);
   background(0);
   stroke(255);
   noLoop();
 }
 
-function draw() {
-  // Draw a line using the global variable "a"
-  line(a, 0, a, height);
+var Walker = function() {
+    this.x = width/2;
+    this.y = height/2;
+};
 
-  // Use a local variable a in for loop
-  for (let a = 120; a < 200; a += 3) {
-    line(a, 0, a, height);
-  }
+Walker.prototype.display = function() {
+    stroke(0, 0, 0);
+    point(this.x, this.y);
+};
 
-  // Make a call to the custom function drawAnotherLine()
-  drawAnotherLine();
+// Randomly move up, down, left, right, or stay in one place
+Walker.prototype.walk = function() {
+    var choice = floor(random(4));
+    if (choice === 0) {
+        this.x++;
+    } else if (choice === 1) {
+        this.x--;
+    } else if (choice === 2) {
+        this.y++;
+    } else {
+        this.y--;
+    }
+};
 
-  // Make a call to the custom function drawYetAnotherLine()
-  drawYetAnotherLine();
-}
+var w = new Walker();
 
-function drawAnotherLine() {
-  // Create a new variable "a" local to this function
-  let a = 320;
-  // Draw a line using the local variable "a"
-  line(a, 0, a, height);
-}
-
-function drawYetAnotherLine() {
-  // Because no new local variable "a" is set,
-  // this line draws using the original global
-  // variable "a" which is set to the value 20.
-  line(a + 3, 0, a + 3, height);
-}
+var draw = function() {
+    w.walk();
+    w.display();
+};
 
 
-//////////////
+//////// exemple qui fonctionne
 
-// var Walker = function() {
-//     this.x = width/2;
-//     this.y = height/2;
-// };
+// let a = 80; // Create a global variable "a"
 //
-// Walker.prototype.display = function() {
-//     stroke(0, 0, 0);
-//     point(this.x, this.y);
-// };
+// function draw() {
+//   // Draw a line using the global variable "a"
+//   line(a, 0, a, height);
 //
-// // Randomly move up, down, left, right, or stay in one place
-// Walker.prototype.walk = function() {
-//     var choice = floor(random(4));
-//     if (choice === 0) {
-//         this.x++;
-//     } else if (choice === 1) {
-//         this.x--;
-//     } else if (choice === 2) {
-//         this.y++;
-//     } else {
-//         this.y--;
-//     }
-// };
+//   // Use a local variable a in for loop
+//   for (let a = 120; a < 200; a += 3) {
+//     line(a, 0, a, height);
+//   }
 //
-// var w = new Walker();
+//   // Make a call to the custom function drawAnotherLine()
+//   drawAnotherLine();
 //
-// var draw = function() {
-//     w.walk();
-//     w.display();
-// };
+//   // Make a call to the custom function drawYetAnotherLine()
+//   drawYetAnotherLine();
+// }
+//
+// function drawAnotherLine() {
+//   // Create a new variable "a" local to this function
+//   let a = 320;
+//   // Draw a line using the local variable "a"
+//   line(a, 0, a, height);
+// }
+//
+// function drawYetAnotherLine() {
+//   // Because no new local variable "a" is set,
+//   // this line draws using the original global
+//   // variable "a" which is set to the value 20.
+//   line(a + 3, 0, a + 3, height);
+// }
